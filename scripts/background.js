89d39307a86c84da7222c03d4e7b622aabe3d3f6
Garanina Natalya <garaninanatalya3@gmail.com>
2017-12-17 21:14:46 +0400

    Initial commit

---
+++ scripts/background.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    data: {
+        url: 'http://reminder.ddns.net/query',
+        enable: true
+    },
+    created: function() {
+        var component = this;
+        setInterval(function cycle() {
+            if(component.enable)
+                axios.get(component.url)
+                    .then(function(response) {
+						console.log(response.data)
+					})
+
+        }, 5000)
+    }
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    app.enable = !app.enable;
+    if(app.enable) alert('Напоминалка активирована!');
+    else alert('Напоминалка деактивированна');
+});
