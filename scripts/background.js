b79d4bc9b36ab1ca73bafac582be93e20987b284
DinarKH <dinarkhalimov@gmail.com>
2017-12-28 20:46:06 +0400

    Update tasks

--- scripts/background.js
+++ scripts/background.js
@@ -11,39 +11,32 @@ setInterval(function cycle() {
     })
         .then(function(response) {
             try{
-            console.log(response);
-            responseGlob=response;
-            //var offset = new Date().getTimezoneOffset();
-            /*offset=120;
-            var displacement=0;
-            if(offset>=0){
-                displacement=(offset+240)*-1;
-            }else{
-                displacement=-240-offset;
-            }
-            var displacement= displacement*60*1000;*/
-            //console.log(displacement);
-            var dateObj=new Date(parseInt(response.data.date));
-            var strDate=dateObj.getFullYear()+" "+(month[dateObj.getMonth()])+" "+dateObj.getDate()+" "+dateObj.getHours()+":"+dateObj.getMinutes();
-
-            var notifOpt={
-                type: "basic",
-                iconUrl: "128.png",
-                title: response.data.topic.name+" "+response.data.title,
-                message: response.data.description+" "+strDate,
-                buttons: [
-                    { title: 'Выполнено' },
-                    { title: 'Отложить' }
-                ]}
-            chrome.notifications.create("Notrif1",notifOpt) 
-            
-        }catch(err){
-            console.log(err);
-            console.log(notifOpt);
-        }
-        
+                console.log(response);
+                responseGlob=response;
+                var dateObj=new Date(parseInt(response.data.notify_date));
+                var hours=dateObj.getHours();
+                if(hours<10)
+                    hours="0"+hours;
+                var minutes=dateObj.getMinutes();
+                if(minutes<10)
+                    minutes="0"+minutes;
+                var strDate=dateObj.getFullYear()+" "+(month[dateObj.getMonth()])+" "+dateObj.getDate()+" "+hours+":"+minutes;
+                var notifOpt={
+                    type: "basic",
+                    iconUrl: "128.png",
+                    title: response.data.topic.name+": "+response.data.title,
+                    message: response.data.description+" "+strDate,
+                    buttons: [
+                        { title: 'Выполнено' },
+                        { title: 'Отложить' }
+                    ]}
+                chrome.notifications.create("Notrif1",notifOpt) 
+                
+            }catch(err){
+                console.log(err);
+            }        
         })
-}, 2000)
+}, 3000)
 
 function deleteTask(id){
     axios.delete('http://reminder.ddns.net/api/tasks/'+id, {
@@ -55,23 +48,33 @@ function deleteTask(id){
             console.log(response);
         })
 }
-function updateTask(id){
-    axios.patch('http://reminder.ddns.net/api/tasks/'+id, {
-    headers: {
-        Authorization: "Bearer " + getToken()
-    }
+
+function updateTask(data){
+    var notif=data.notify_date;
+    console.log(data.minuts);
+    notif=parseInt(notif)+data.minuts*60*1000;
+    console.log(notif);
+axios({
+        method: 'put',
+        url: 'http://reminder.ddns.net/api/tasks/'+data.id,
+        headers: {
+            Authorization: "Bearer " + getToken(),
+            notifyDate: notif
+        }
     })
         .then(function(response) {
-            console.log(response);
-        })
+        console.log(response);
+        console.log(data.notify_date)
+    });
 }
+
 chrome.notifications.onButtonClicked.addListener(function callbackD(notificationId,buttonIndex){
     if(buttonIndex==0){
         console.log("Выполнено")
         deleteTask(responseGlob.data.id)
     }else if(buttonIndex==1){
         console.log("Отложить")
-        updateTask(responseGlob.data.id)
+        updateTask(responseGlob.data)
     }
 });
 function getToken() {
