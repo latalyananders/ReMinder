8465b10a7a671e67d3dcdda11b484cf0df563cd2
Islam Shigabutdinov <islam2332@gmail.com>
2017-12-29 11:58:49 +0400

    hot fix

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,18 @@
 var responseGlob;
-<<<<<<< HEAD
 console.log("Privet");
-=======
->>>>>>> Dinar
+
 setInterval(function cycle() {
     var month =['январь','февраль','март','апрель','май','июнь','июль','август','сентябрь',
-        'октябрь','ноябрь','декабрь']
-    var notifOpt
+        'октябрь','ноябрь','декабрь'];
+    var notifOpt;
     axios.get('http://reminder.ddns.net/api/query', {
         headers: {
             Authorization: "Bearer " + getToken()
         }
     })
         .then(function(response) {
-            try{
-<<<<<<< HEAD
-                //merge
-=======
->>>>>>> Dinar
+            if(response.data != "")
+                try{
                 console.log(response);
                 responseGlob=response;
                 var dateObj=new Date(parseInt(response.data.notify_date));
@@ -28,12 +23,16 @@ setInterval(function cycle() {
                 if(minutes<10)
                     minutes="0"+minutes;
                 var strDate=dateObj.getFullYear()+" "+(month[dateObj.getMonth()])+" "+dateObj.getDate()+" "+hours+":"+minutes;
-<<<<<<< HEAD
+
+                var description = "";
+                if(response.data.description!=null)
+                    description = response.data.description;
+
                 var notifOpt={
                     type: "basic",
                     iconUrl: "128.png",
                     title: response.data.topic.name+": "+response.data.title,
-                    message: response.data.description+" "+strDate,
+                    message: description+" "+strDate,
                     buttons: [
                         { title: 'Выполнено' },
                         { title: 'Отложить' }
@@ -41,10 +40,9 @@ setInterval(function cycle() {
                 chrome.notifications.create("Notrif1",notifOpt)
 
             }catch(err){
-=======
                 var description = "";
                 if(response.data.description != null)
-                    description = response.data.description
+                    description = response.data.description;
                 var notifOpt={
                     type: "basic",
                     iconUrl: "128.png",
@@ -56,11 +54,7 @@ setInterval(function cycle() {
                         { title: 'Отложить на час' }
                     ]}
                     chrome.notifications.create("notifRegul",notifOpt)
-                }               
-            catch(err){
->>>>>>> Dinar
-                console.log(err);
-            }
+                }
         })
 }, 3000)
 
@@ -68,33 +62,10 @@ function deleteTask(id){
     axios.delete('http://reminder.ddns.net/api/tasks/'+id, {
         headers: {
             Authorization: "Bearer " + getToken()
-<<<<<<< HEAD
         }
     })
         .then(function(response) {
             console.log(response);
-        })
-}
-
-function updateTask(data){
-    var notif=data.notify_date;
-    console.log(data.minuts);
-    notif=parseInt(notif)+data.minuts*60*1000;
-    console.log(notif);
-    axios({
-        method: 'put',
-        url: 'http://reminder.ddns.net/api/tasks/'+data.id,
-        headers: {
-            Authorization: "Bearer " + getToken(),
-            notifyDate: notif
-=======
->>>>>>> Dinar
-        }
-    })
-        .then(function(response) {
-            console.log(response);
-<<<<<<< HEAD
-=======
         })
 }
 
@@ -111,29 +82,19 @@ function updateTask(data,minutes){
     })
         .then(function(response) {
             console.log(response);
->>>>>>> Dinar
             console.log(data.notify_date)
         });
 }
 
 chrome.notifications.onButtonClicked.addListener(function callbackD(notificationId,buttonIndex){
-<<<<<<< HEAD
     if(buttonIndex==0){
         console.log("Выполнено")
         deleteTask(responseGlob.data.id)
     }else if(buttonIndex==1){
         console.log("Отложить")
-        updateTask(responseGlob.data)
+        updateTask(responseGlob.data,60)
     }
-=======
-        if(buttonIndex==0){
-            console.log("Выполнено")
-            deleteTask(responseGlob.data.id)
-        }else if(buttonIndex==1){
-            console.log("Отложить на час")
-            updateTask(responseGlob.data,60)
-        }
->>>>>>> Dinar
+
 });
 function getToken() {
     var token = localStorage.getItem('token');
